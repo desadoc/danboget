7cc379c62a623f9dbbd0dcc1d0c467c8a3102cad
Erivaldo Filho <desadoc@gmail.com>
2017-09-09 23:48:42 -0300

    Now a modal can be closed by clicking outside the panel

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,10 +5,22 @@ import CN from 'classnames';
 import Button from './Button';
 
 class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    if (!this.props.onExitClick)
+      return;
+    if (!e.target.classList.contains('modal-overlay'))
+      return;
+    this.props.onExitClick();
+  }
   render() {
     return (
       <div className={CN("modal", this.props.className)}>
-        <div className="modal-overlay">
+        <div className="modal-overlay" onClick={this.handleClick}>
           <div className="modal-panel">
             {this.props.children}
             <Button className="dismiss-button"
