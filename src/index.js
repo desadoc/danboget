9b5eb738b9d98c7e94996f05a5b5426436bad6f3
Erivaldo Filho <desadoc@gmail.com>
2017-09-14 17:56:20 -0300

    Renamed App as Search

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './style/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const AppWRouter =
-  (props) =>
-    <Router>
-      <div className="router-root">
-        <Route exact path="/" render={(props) => <Redirect to="/welcome" />} />
-        <Route exact path="/welcome" render={
-          (props) => <Redirect to="/search?limit=100&filters=super_safe" />
-        } />
-        <Route exact path="/search" component={App} />
-      </div>
-    </Router>;
-
-ReactDOM.render(<AppWRouter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
